f389efc7326c6c16fbdb9271a75b15cc5ac3c7a7
Celestial Jerusha <jerushaamuri@gmail.com>
2023-09-27 21:14:51 +0530

    submitting code-Wed Sep 27 2023 21:14:49 GMT+0530 (India Standard Time)

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,11 @@
 // write code for Welcome component here
+import React from "react"; 
+
+const Welcome = ({name}) =>(
+    <div>
+        <h1>Hey ! {name}</h1>
+        <p>Welcome to School.</p>
+    </div>
+)
+
+export default Welcome
