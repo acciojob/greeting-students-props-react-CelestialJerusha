4c452a190b83ac112ead42141eeb24acba959d61
Jerusha <62611035+CelestialJerusha@users.noreply.github.com>
2023-11-08 20:18:01 +0000

    commit

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,10 +2,10 @@
 import React from "react"; 
 
 const Welcome = ({name}) =>(
-    <div>
+    <p>
         <h1>Hey ! {name}</h1> 
         <h2>Welcome to School.</h2>
-    </div>
+    </p>
 )
 
 export default Welcome
